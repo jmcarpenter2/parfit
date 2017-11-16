1966676e629da75350338490f38fcae8a1a6d577
jmcarpenter2 <jmcarpenter2@dons.usfca.edu>
2017-11-16 11:26:37 -0800

    A function for fitting models in parallel over a ParameterGrid

---
+++ parallelizeFit.py
@@ -0,0 +1,46 @@
+from joblib import Parallel, delayed
+
+
+def fitOne(model, X, y, params):
+    """
+    Makes one model fit using provided data and parameters
+    :param model: The function name of the model you wish to pass,
+        e.g. LogisticRegression [NOTE: do not instantiate with ()]
+    :param X: The independent variable data
+    :param y: The response variable data
+    :param params: The parameters passed through to the model from the parameter grid
+    :return: Returns the fitted model
+    """
+    m = model(**params)
+    return m.fit(X, y)
+
+
+def parallelizeFit(model, paramGrid, X, y, n_jobs=-1, verbose=10):
+    """
+    Parallelizes fitting all model using combinations of parameters in paramGrid on provided data.
+    :param model: The function name of the model you wish to pass,
+        e.g. LogisticRegression [NOTE: do not instantiate with ()]
+    :param paramGrid: The ParameterGrid object created from sklearn.model_selection
+    :param X: The independent variable data
+    :param y: The response variable data
+    :param n_jobs: Number of cores to use in parallelization (defaults to -1: all cores)
+    :param verbose: The level of verbosity of reporting updates on parallel process
+        Default is 10 (send an update at the completion of each job)
+    :return: Returns a list of fitted models
+
+    Example usage:
+        from sklearn.linear_model import LogisticRegression
+        from sklearn.model_selection import ParameterGrid
+        model = LogisticRegression
+        grid = {
+            'C': [1e-4, 1e-3], # regularization
+            'penalty': ['l1','l2'], # penalty type
+            'n_jobs': [-1] # parallelize within each fit over all cores
+        }
+        paramGrid = ParameterGrid(grid)
+        myModels = parallelizeFit(model, paramGrid, X, y)
+    """
+    return Parallel(n_jobs=n_jobs, verbose=verbose)(delayed(fitOne)(model,
+                                                           X,
+                                                           y,
+                                                           params) for params in paramGrid)
