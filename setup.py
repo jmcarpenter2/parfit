0ec451a4a72a97503712ab2ee8ff36e1346b4b85
jmcarpenter2 <jmcarpenter2@dons.usfca.edu>
2017-11-27 12:55:33 -0800

    Release 0.14 with fixes for 1d plotting

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'parfit',
   packages = ['parfit'], # this must be the same as the name above
-  version = '0.13',
+  version = '0.14',
   description = 'A package for parallelizing the fit and flexibly scoring of sklearn machine learning models, with optional plotting routines.',
   author = 'Jason Carpenter',
   author_email = 'jmcarpenter2@dons.usfca.edu',
   url = 'https://github.com/jmcarpenter2/parFit', # use the URL to the github repo
-  download_url = 'https://github.com/jmcarpenter2/parFit/archive/0.13.tar.gz', 
+  download_url = 'https://github.com/jmcarpenter2/parFit/archive/0.14.tar.gz', 
   keywords = ['parallelize', 'sklearn', 'machinelearning','models', 'fit'], # arbitrary keywords
   install_requires = [
     'joblib',
