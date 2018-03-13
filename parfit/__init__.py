1494cc9b7cb4ec810929c42280b4334dbb5fe535
jmcarpenter2 <jmcarpenter2@dons.usfca.edu>
2018-03-13 11:15:53 -0700

    Update init

--- parfit/__init__.py
+++ parfit/__init__.py
@@ -2,5 +2,6 @@ from . import plot
 from . import fit
 from . import parfit
 from . import score
+from . import crossval
 
-__all__ = (plot.__all__ + fit.__all__ + score.__all__ + parfit.__all__)
+__all__ = (plot.__all__ + fit.__all__ + score.__all__ + crossval.__all__ + parfit.__all__)
