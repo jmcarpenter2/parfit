0b61f20fdf073aab6f6215263c21515b2e5b2968
jmcarpenter2 <jmcarpenter2@dons.usfca.edu>
2018-03-17 08:04:50 -0700

    Version 0.182

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name='parfit',
     packages=['parfit'],  # this must be the same as the name above
-    version='0.181',
+    version='0.182',
     description='A package for parallelizing the fit and flexibly scoring of sklearn machine learning models, with optional plotting routines.',
     author='Jason Carpenter',
     author_email='jmcarpenter2@dons.usfca.edu',
     url='https://github.com/jmcarpenter2/parfit',  # use the URL to the github repo
-    download_url='https://github.com/jmcarpenter2/parfit/archive/0.181.tar.gz',
+    download_url='https://github.com/jmcarpenter2/parfit/archive/0.182.tar.gz',
     keywords=['parallelize', 'sklearn', 'machinelearning',
               'models', 'fit'],  # keywords
     install_requires=[
